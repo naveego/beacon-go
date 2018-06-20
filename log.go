887d71077220993994fd3e19e5c0266003f00086
SteveRuble <SteveRuble@users.noreply.github.com>
2018-06-20 17:18:57 -0400

    added tests for monitor

--- log.go
+++ log.go
@@ -7,32 +7,32 @@ import (
 // Log is the logging abstraction for the beacon client.
 type Log interface {
 	// Debug logs a debug message.
-	Debug(source NRN, msg string, data ...interface{})
+	Debug(source NRN, msg string, data ...map[string]interface{})
 	// Warn logs a warning message.
-	Warn(source NRN, msg string, data ...interface{})
+	Warn(source NRN, msg string, data ...map[string]interface{})
 	// Error logs an error message.
-	Error(source NRN, msg string, err error, data ...interface{})
+	Error(source NRN, msg string, err error, data ...map[string]interface{})
 }
 
 type ConsoleLog struct{}
 
-func (c ConsoleLog) Debug(source NRN, msg string, data ...interface{}) {
+func (c ConsoleLog) Debug(source NRN, msg string, data ...map[string]interface{}) {
 	log.Printf("DBG %s - %q  %#v", source, msg, data)
 }
-func (c ConsoleLog) Warn(source NRN, msg string, data ...interface{}) {
+func (c ConsoleLog) Warn(source NRN, msg string, data ...map[string]interface{}) {
 	log.Printf("WRN %s - %q  %#v", source, msg, data)
 
 }
-func (c ConsoleLog) Error(source NRN, msg string, err error, data ...interface{}) {
+func (c ConsoleLog) Error(source NRN, msg string, err error, data ...map[string]interface{}) {
 	log.Printf("ERR %s - %q: %s %#v", source, msg, err, data)
 }
 
 type EmptyLog struct{}
 
-func (c EmptyLog) Debug(source NRN, msg string, data ...interface{}) {
+func (c EmptyLog) Debug(source NRN, msg string, data ...map[string]interface{}) {
 }
-func (c EmptyLog) Warn(source NRN, msg string, data ...interface{}) {
+func (c EmptyLog) Warn(source NRN, msg string, data ...map[string]interface{}) {
 
 }
-func (c EmptyLog) Error(source NRN, msg string, err error, data ...interface{}) {
+func (c EmptyLog) Error(source NRN, msg string, err error, data ...map[string]interface{}) {
 }
